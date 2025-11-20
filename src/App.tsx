f5659b7e0a3eeb6685b30bea61b1099fa46eb8be
Rathapol.K <>
2025-11-20 10:22:39 +0700

    feat: enhance theme configuration for MuiTextField with default props and styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,30 @@ import { DialogsProvider } from './lib'
 import { router } from './router'
 import { RouterProvider } from '@tanstack/react-router'
 
-const theme = createTheme({})
+const theme = createTheme({
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        size: 'small',
+        slotProps: {
+          inputLabel: {
+            shrink: true,
+          },
+        },
+        sx: {
+          '&[data-isdirty]:not([data-isdefaultvalue]) .MuiOutlinedInput-root fieldset':
+            {
+              borderColor: 'success.light',
+            },
+          '&[data-isdirty]:not([data-isvalid]) .MuiOutlinedInput-root fieldset':
+            {
+              borderColor: 'error.main',
+            },
+        },
+      },
+    },
+  },
+})
 
 export default function App() {
   return (
